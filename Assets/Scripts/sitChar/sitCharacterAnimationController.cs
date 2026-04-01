688328db902a7f25c3f780e079df723bacc7ffb1
MUY <oncwnuMCGTjZPlkVXDeAGCntZ15Y@git.weixin.qq.com>
2026-04-01 22:49:13 +0800

    chore: strengthen obsolete warning for sitCharacterAnimationController
    - Change to error-level obsolete warning (second parameter = true)
    - Forces migration to AnimationStateMachine
    - Provides clear migration message with documentation link

--- Assets/Scripts/sitChar/sitCharacterAnimationController.cs
+++ Assets/Scripts/sitChar/sitCharacterAnimationController.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 using System;
+using System.Collections;
 
-[System.Obsolete("Use CharacterAnimationController with sitting.json config instead. This class will be removed in v2.0")]
+[System.Obsolete(
+    "This class is deprecated and will be removed in v2.0. " +
+    "Use AnimationStateMachine instead. " +
+    "See migration guide: docs/migration/animation-system-v2.md",
+    true)]
 public class sitCharacterAnimationController : MonoBehaviour, ICharacterAnimation
 {
     [SerializeField] private BloodEffectController bloodEffectController;
@@ -12,8 +17,44 @@ public class sitCharacterAnimationController : MonoBehaviour, ICharacterAnimatio
     void Awake()
     {
         _innerController = gameObject.AddComponent<CharacterAnimationController>();
+    }
+    
+    void Start()
+    {
+        enabled = false;
         
-        // 从AnimationService获取sitting配置
+        AnimationService.Instance.OnConfigChanged += OnAnimationConfigChanged;
+        
+        StartCoroutine(InitializeConfig());
+    }
+    
+    void OnAnimationConfigChanged(AnimationConfig config)
+    {
+        if (config != null)
+        {
+            _innerController.SetConfig(config);
+            Debug.Log("[sitCharacterAnimationController] Config loaded from AnimationService event");
+        }
+    }
+    
+    void OnDestroy()
+    {
+        if (AnimationService.Instance != null)
+        {
+            AnimationService.Instance.OnConfigChanged -= OnAnimationConfigChanged;
+        }
+    }
+    
+    public void InitializeFromAnimationService(string configName)
+    {
+        AnimationService.Instance.SetCharacter(_innerController, configName);
+        Debug.Log($"[sitCharacterAnimationController] Initialized with config: {configName}");
+    }
+    
+    IEnumerator InitializeConfig()
+    {
+        yield return null;
+        
         var sittingConfig = AnimationService.Instance.CurrentConfig;
         _innerController.SetConfig(sittingConfig);
         
@@ -26,11 +67,6 @@ public class sitCharacterAnimationController : MonoBehaviour, ICharacterAnimatio
         bloodTextField?.SetValue(_innerController, bloodTextController);
         
         Debug.Log("[sitCharacterAnimationController] Migrated to CharacterAnimationController with sitting config from AnimationService");
-    }
-    
-    void Start()
-    {
-        enabled = false;
     }
     
     public void UpdateAnimationState(int newState)
@@ -51,6 +87,16 @@ public class sitCharacterAnimationController : MonoBehaviour, ICharacterAnimatio
     public void PlayByEmotionCode(int emotionCode)
     {
         _innerController?.PlayByEmotionCode(emotionCode);
+    }
+    
+    public void PlayByName(string animationName)
+    {
+        _innerController?.PlayByName(animationName);
+    }
+    
+    public void TriggerBloodEffect()
+    {
+        _innerController?.TriggerBloodEffect();
     }
     
     public void PlayBend() => PlayAnimation("bend");
